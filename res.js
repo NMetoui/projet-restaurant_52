f0d83ac2d6635d147d6587e89bd0c88daeb0d6e8
NMetoui <88581423+NMetoui@users.noreply.github.com>
2021-09-11 10:28:08 +0100

    Add files via upload

---
+++ res.js
@@ -0,0 +1,14 @@
+var aj = document.getElementById("ajout");
+
+aj.addEventListener("click", function () {
+  console.log("aj=", aj);
+});
+/*for (let i = 0; i < add.length; i++) {
+  let de = add[i];
+  de.addEventListener("click", function () {
+    //de.parentElement.remove();
+    console.log(prices);
+    //console.log(de.previousElementSibling.innerHTML);
+    //var delprice = prixtotal.value - de.previousElementSibling.innerHTML;
+    //prixtotal.value = delprice;
+  });*/
